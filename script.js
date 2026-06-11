a49213b2c5ad3f475fd6c38b61e6d64c0bd4883a
Alejandro Atehortua <ateo05@gmail.com>
2026-06-11 21:13:48 +0200

    hover animations

--- script.js
+++ script.js
@@ -258,7 +258,7 @@ function createHotspots(visor) {
       new THREE.MeshBasicMaterial({
           color: 0xff0000,
           transparent: true,
-          opacity: 0,
+          opacity: 0.15,
           side: THREE.DoubleSide
       });
 
@@ -267,7 +267,7 @@ function createHotspots(visor) {
       new THREE.MeshBasicMaterial({
           color: 0x00ff00,
           transparent: true,
-          opacity: 0,
+          opacity: 0.15,
           side: THREE.DoubleSide
       });
 
@@ -276,7 +276,7 @@ function createHotspots(visor) {
       new THREE.MeshBasicMaterial({
           color: 0x0000ff,
           transparent: true,
-          opacity: 0,
+          opacity: 0.15,
           side: THREE.DoubleSide
       });
 
